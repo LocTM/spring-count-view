1ebcce15ce1b363dd952129641a6e3d5bb1ccd41
LocTM <tranminhloc.anhk@gmail.com>
2024-03-08 08:23:47 +0700

    done

--- src/main/java/com/codegym/configuration/AppInitializer.java
+++ src/main/java/com/codegym/configuration/AppInitializer.java
@@ -2,7 +2,7 @@ package com.codegym.configuration;
 
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
     protected Class<?>[] getRootConfigClasses() {
         return new Class[]{AppConfiguration.class};
